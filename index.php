69bce823f742e41779a80ab7b4026fa259f7fbe2
Omed Habib <omedhabib@gmail.com>
2014-08-05 20:00:14 -0700

    Setting the user_id as a cookie

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+
+// -----------------------------------
+// Checkout the "shopping cart"
+// -----------------------------------
+
 require('cart.php');
 
 $Cart = new Cart();
@@ -7,9 +12,28 @@ $Cart = new Cart();
 $amount = rand(1,50);
 
 echo $Cart->checkout($amount);
+// -----------------------------------
+
+
+
+// -----------------------------------
+// Set a cookie
+// -----------------------------------
+
+$user_id = rand(1000, 9999);
+setcookie("user_id", $user_id, time()+7200);
+
+echo "<br>";
+echo "Cookie: " . $_COOKIE["user_id"];
+echo "<br>";
+
+// -----------------------------------
 
 
 	
+// -----------------------------------
+// Curl our Node.js app
+// -----------------------------------
 
 // Get cURL resource
 $curl = curl_init();
@@ -25,5 +49,7 @@ $resp = curl_exec($curl);
 
 // Close request to clear up some resources
 curl_close($curl);
+// -----------------------------------
+
 
 ?>
